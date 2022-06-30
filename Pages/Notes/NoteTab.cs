05990ab910edc025eceedebd132dde66836197b1
hasherr <eklein217@gmail.com>
2022-06-30 08:39:48 -0400

    Some confusion

--- Pages/Notes/NoteTab.cs
+++ Pages/Notes/NoteTab.cs
@@ -1,6 +1,9 @@
+using MudBlazor;
+
 public class NoteTab
 {
 	public Note Note { get; set; }
+	public Color IconColor {get; set;} = Color.Default;
 
 	public NoteTab(Note note)
 	{
--- Shared/Note.cs
+++ Shared/Note.cs
@@ -1,6 +1,6 @@
 public class Note
 {
     public Guid Id { get; set; } = Guid.NewGuid();
-    public string Title { get; set; }
-    public string Content { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
 }
