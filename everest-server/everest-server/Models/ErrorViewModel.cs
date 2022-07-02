cb4059559642a7a0e6fe1b12e40deb108d2991e5
hasherr <eklein217@gmail.com>
2022-07-02 15:13:33 -0400

    Revert "Added server project"
    This reverts commit d6a739c9d69689754f9fe59ea84cfbcb3edb133d.

--- everest-server/everest-server/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace everest_server.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
