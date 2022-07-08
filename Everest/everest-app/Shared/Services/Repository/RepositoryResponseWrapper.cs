dd1db987abaa9df6deab854c9fec758e277050e1
hasherr <eklein217@gmail.com>
2022-07-08 14:41:29 -0400

    UI improvements for note title bug

--- Everest/everest-app/Shared/Services/Repository/RepositoryResponseWrapper.cs
+++ Everest/everest-app/Shared/Services/Repository/RepositoryResponseWrapper.cs
@@ -3,7 +3,7 @@ namespace everest_app.Shared.Services.Repository
 {
     public class RepositoryResponseWrapper<TResponseObject>
     {
-        public bool Success { get; set; } = true,
+        public bool Success { get; set; } = true;
         public TResponseObject? Value { get; set; }
         public RepositoryResponseError? Error { get; set; }
     }
