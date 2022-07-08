46065780c6ceb04fa91478706b8489f2d4755642
hasherr <eklein217@gmail.com>
2022-07-08 15:10:18 -0400

    Added TODO migrations

---
+++ everest-common/Models/ToDoItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace everest_common.Models
+{
+    public class ToDoItem
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [NotMapped]
+        public string UpdatedName { get; set; } = string.Empty;
+
+        public bool Complete { get; set; } = false;
+
+        public DateTime TimeCompleted { get; set; }
+
+        public ToDoItem()
+        {
+
+        }
+    }
+}
+
