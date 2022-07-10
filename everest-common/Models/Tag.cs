bda6fe6ace8d890bef8bcd67ad8aa58b20dba391
hasherr <eklein217@gmail.com>
2022-07-10 16:46:08 -0400

    Added Tag model

---
+++ everest-common/Models/Tag.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace everest_common.Models
+{
+    public class Tag
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public DateTime DateCreated { get; set; }
+
+        public string OwnerId { get; set; }
+        public IdentityUser Owner { get; set; }
+    }
+}
+
